5071f72db2a70a50ac104bc84461df84f4103d36
BlackPymer <g39palchevski@gmail.com>
2025-05-30 08:08:35 +0300

    Fixed physics issues

--- src/main.cpp
+++ src/main.cpp
@@ -2,37 +2,23 @@
 #include "../include/physics/physics_engine.hpp"
 #include "../include/graphics/graphics_engine.hpp"
 #include <SFML/Window.hpp>
-void oneBigAndOneSmall()
+PhysicsEngine oneBigAndOneSmall()
 {
     PhysicsEngine physicsEngine;
-    physicsEngine.createBall(1e6, 1e24, {0, 0});            // Central heavy mass
-    physicsEngine.createBall(1e5, 1e3, {0, 1e8}, {1e3, 0}); // Orbiting satellite
+    physicsEngine.createBall(1e4, 1e24, {500, 500});      // Central heavy mass
+    physicsEngine.createBall(1e3, 1e3, {0, 0}, {1e3, 0}); // Orbiting satellite
 
-    unsigned long long frame = 0;
-    while (true)
-    {
-        frame++;
-        if (frame % 100000 == 0)
-            std::cout << physicsEngine.getLogs();
-        physicsEngine.update();
-    }
+    return physicsEngine;
 }
 
-void threeSmall()
+PhysicsEngine threeSmall()
 {
     PhysicsEngine physicsEngine;
     physicsEngine.createBall(1e2, 1e3, {0, 1e4});
     physicsEngine.createBall(1e2, 1e3, {1e1, -1e4});
     physicsEngine.createBall(1e2, 1e3, {-1e2, -1e3});
 
-    unsigned long long frame = 0;
-    while (true)
-    {
-        frame++;
-        if (frame % 10000 == 0)
-            std::cout << physicsEngine.getLogs();
-        physicsEngine.update();
-    }
+    return physicsEngine;
 }
 
 int main()
@@ -40,9 +26,7 @@ int main()
     sf::RenderWindow window(sf::VideoMode({1280, 720}), "Physics simulator");
     GraphicsEngine graphicsEngine(window);
 
-    PhysicsEngine physicsEngine;
-    physicsEngine.createBall(1e2, 1e3, {0, 0});
-    physicsEngine.createBall(1e2, 1e2, {400, 400});
+    PhysicsEngine physicsEngine(oneBigAndOneSmall());
 
     while (window.isOpen())
     {
